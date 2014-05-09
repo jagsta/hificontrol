9f0e7fbded773c3f6abe0a21542d8b0136105890
Jag Bains <git@jagbains.org>
2014-05-09 22:14:20 +0100

    Many changes, now using pins 5 and 6 for Vcc on H Bridge to allow setting LOW when idle to free stepper

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,7 +4,6 @@
 // IR receives on pin 7
 IRrecv irrecv(7); 
 decode_results results;
-int last;
 
 // change this to the number of steps on your motor
 #define STEPS 200
@@ -14,48 +13,55 @@ int last;
 // attached to
 Stepper stepper(STEPS, 8,9,10,11);
 
-// the previous reading from the analog input
+int countdown = 1000;
 
 void setup()
 {
   // set the speed of the motor to 30 RPMs
-  stepper.setSpeed(15);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  stepper.setSpeed(60);
+  stepper.step(-540);
+  stepper.step(100);
+  stepper.setSpeed(20);
   irrecv.enableIRIn(); // enable IR receiver
 }
 
 void loop()
 {
   if (irrecv.decode(&results)) {
-    if (results.value == 0xFFA857) {
+    if (results.value == 0x217C346) {
     //  Serial.println("DOWN");
-      stepper.step(-1);
-      last = 1;
+      digitalWrite(5,HIGH);
+      digitalWrite(6,HIGH);
+      stepper.step(-2);
+      countdown = 10000;
     }
-    else if (results.value == 0xFF906F) {
+    else if (results.value == 0x6E283FF1) {
      // Serial.println("UP");
-      stepper.step(1);
-      last = 2;
+      digitalWrite(5,HIGH);
+      digitalWrite(6,HIGH);
+      stepper.step(2);
+      countdown = 10000;
     }
-    else if (results.value == 0xFFFFFFFF) {
-      Serial.print("REPEAT ");
-      switch ( last ) {
-        case 0:
-         // Serial.println("IGNORE");
-          break;
-        case 1:
-         // Serial.println("DOWN");
-          stepper.step(-1);
-          break;
-        case 2:
-         // Serial.println("UP");
-          stepper.step(1);
-          break;
-      }
-    }
-    else {
-      // Serial.println("IGNORE");
-      last = 0;
+    else if (results.value == 0xDF2FAD9B) {
+      // Serial.println("INITIALISE");
+      digitalWrite(5,HIGH);
+      digitalWrite(6,HIGH);
+      stepper.setSpeed(60);
+      stepper.step(-540);
+      stepper.step(100);
+      stepper.setSpeed(20);
+      countdown = 10000;
     }
     irrecv.resume(); // Continue receiving
+  }
+  countdown--;
+  delay(1);
+  if (countdown<1) {
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
   }
 }
